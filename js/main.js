a3dd10fc73dd9cbc654bac0e3222d9688a7e9857
Casper Moyo <cassymoyo@gmail.com>
2022-12-14 07:47:01 -0800

    mobile

--- js/main.js
+++ js/main.js
@@ -13,5 +13,34 @@ document.addEventListener(
                 }
             }
         )
+    let cardParent = document.querySelector('.products-page')
+    cardParent.querySelectorAll('.card').forEach(card => {
+        card.onclick = function(){
+            let products = document.querySelector('.products-page')
+            products.querySelector('.content').style.display = 'none'
+            show(this.dataset.page)
+        }
+    })
+
+    // footer date
+    const d = document.getElementById('date')
+    let ft = new Date()
+    d.innerHTML = ft.getFullYear();
     }
 )
+
+function show(cardId) {
+    let products = document.querySelector('.products-page')
+    document.querySelector('#soft').style.display = 'none'
+    document.querySelector('#mob').style.display = 'none'
+    document.querySelector('#web').style.display = 'none'
+    
+    products.querySelector('.content').style.display = 'none'
+    try {
+        document.querySelector(`#${cardId}`).style.display = 'block'
+    } catch (error) {
+        // not to return error
+    }
+    
+    
+}
